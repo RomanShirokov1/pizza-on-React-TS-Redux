920b87572f234d2e792363d9907bf0b078b22e1a
RomanShirokov1 <r0man_sh@mail.ru>
2024-02-11 11:37:08 +0300

    sorting and categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,22 +8,35 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({ name: 'популярности', sortProperty: 'rating' });
+  const [orderType, setOrderType] = React.useState(true);
 
   React.useEffect(() => {
-    fetch('https://066bcc1325295add.mokky.dev/items')
+    setIsLoading(true);
+    fetch(
+      `https://066bcc1325295add.mokky.dev/items?${
+        categoryId > 0 ? `category=${categoryId}` : ''
+      }&sortBy=${orderType ? '' : '-'}${sortType.sortProperty}`,
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType, orderType]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onClickCategory={(id) => setCategoryId(id)} />
+        <Sort
+          value={sortType}
+          onChangeSort={(id) => setSortType(id)}
+          orderValue={orderType}
+          onChangeOrder={() => setOrderType(!orderType)}
+        />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
